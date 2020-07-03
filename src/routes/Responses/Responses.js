218cc830f964974eafd0aa9b1b62f08f7b137d79
EduardoMontanha <edumontanha14@gmail.com>
2020-07-03 02:44:37 -0300

    Create responses components

---
+++ src/routes/Responses/Responses.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Error400 () {
+    return(
+        <main></main>
+    );
+}
+
+function Error403 () {
+    return(
+        <main></main>
+    );
+}
+
+function Error404 () {
+    return(
+        <main></main>
+    );
+}
+
+function Error500 () {
+    return(
+        <main></main>
+    );
+}
+
+function Error503 () {
+    return(
+        <main></main>
+    );
+}
+
+export {
+    Error400,
+    Error403,
+    Error404,
+    Error500,
+    Error503
+}
