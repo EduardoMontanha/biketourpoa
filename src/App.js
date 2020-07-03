e3d2533af70e6c04db36ca5d0060711db99a7705
EduardoMontanha <edumontanha14@gmail.com>
2020-07-03 02:32:18 -0300

    Add header and footer

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import {
     Route
 } from 'react-router-dom';
 
+import Header from './routes/components/Header';
+import Footer from './routes/components/Footer';
 import Landing from './routes/Landing/Landing';
+
 
 function App() {
     return (
         <BrowserRouter>
+            <Header />
 
             <Switch>
                 <Route path="/" component={Landing} />
             </Switch>
+
+            <Footer />
         </BrowserRouter>
     );
 }
