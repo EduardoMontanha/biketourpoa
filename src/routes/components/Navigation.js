247e4cb2dc5053b8cdd226ba070b4e7231b1e087
EduardoMontanha <edumontanha14@gmail.com>
2020-07-03 19:49:04 -0300

    Change class for function

--- src/routes/components/Navigation.js
+++ src/routes/components/Navigation.js
@@ -18,48 +18,46 @@ function openMenu() {
 /**
  * Components
  */
-class Navigation extends React.Component {
+function Navigation () {
 
-    render () {
-        return(
-            <nav id="navigation">
-                <div className="container">
-                    <Link to="/" id="logo"><img src={logo} alt="Logo Bike Tour Poa"/></Link>
+    return(
+        <nav id="navigation">
+            <div className="container">
+                <Link to="/" id="logo"><img src={logo} alt="Logo Bike Tour Poa"/></Link>
 
-                    <button className="nav-bar-toggler" onClick={openMenu}>
-                        <div id="burger-icon">
-                            <div className="bar bar1"></div>
-                            <div className="bar bar2"></div>
-                            <div className="bar bar3"></div>
-                        </div>
-                    </button>
-
-                    <div className="nav-links">
-                        <ul>
-                            <li className="nav-item">
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/roteiros">Roteiros</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/clube">VIPs</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/sobre">Sobre</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/contato">Contato</Link>
-                            </li>
-                            <li className="nav-item special">
-                                <a href="#" target="_blank">Pop Tour</a>
-                            </li>
-                        </ul>
+                <button className="nav-bar-toggler" onClick={openMenu}>
+                    <div id="burger-icon">
+                        <div className="bar bar1"></div>
+                        <div className="bar bar2"></div>
+                        <div className="bar bar3"></div>
                     </div>
+                </button>
+
+                <div className="nav-links">
+                    <ul>
+                        <li className="nav-item">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/roteiros">Roteiros</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/clube">VIPs</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/sobre">Sobre</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/contato">Contato</Link>
+                        </li>
+                        <li className="nav-item special">
+                            <a href="#" target="_blank">Pop Tour</a>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        );
-    }
+            </div>
+        </nav>
+    );
 }
 
 export default Navigation;
