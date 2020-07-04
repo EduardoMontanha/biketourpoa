27143d563a49f5c4462ae1c35cb6e7ebeb63bb2d
EduardoMontanha <edumontanha14@gmail.com>
2020-07-03 23:13:00 -0300

    Create new routes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import {
 
 import Footer from './routes/components/Footer';
 import Landing from './routes/Landing';
-import { Error404 } from './routes/Responses';
+import {
+    UnderConstruction,
+    Error404,
+}
+from './routes/Responses';
 import Navigation from './routes/components/Navigation';
 
 
@@ -18,6 +22,10 @@ function App() {
 
             <Switch>
                 <Route exact path="/" component={Landing} />
+                <Route exact path="/roteiros" component={UnderConstruction} />
+                <Route exact path="/clube" component={UnderConstruction} />
+                <Route exact path="/sobre" component={UnderConstruction} />
+                <Route exact path="/contato" component={UnderConstruction} />
                 <Route path="" component={Error404} />
             </Switch>
 
