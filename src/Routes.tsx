a7e5c8074465cbc54d2ee65d38f02766ab5ecaeb
EduardoMontanha <edumontanha14@gmail.com>
2022-07-07 20:38:04 -0300

    Criando rota para detalhes do roteiro

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,14 @@
 import { Routes as Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
 
 const Routes = () => {
 
     return (
         <Switch>
             <Route path="/" element={<Home />} />
+            <Route path="/roteiro/:id" element={<ProductDetail />} />
+            <Route path="*" element={<Home />} />
         </Switch>
     );
 }
