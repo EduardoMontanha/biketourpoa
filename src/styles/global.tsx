694be21e7268bf2353de5507a9561f48ba5e84ce
EduardoMontanha <edumontanha14@gmail.com>
2022-07-08 12:25:16 -0300

    Added color pallet

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -3,11 +3,15 @@ import './reset.css';
 
 export default createGlobalStyle`
     :root {
-        
+        --dark-grey: #313638;
+        --green: #209052;
+        --light-grey: #E8E9EB;
+        --pink: #EB1265;
+        --yellow: #EFEF16;
     }
 
     body {
-
+        background-color: var(--light-grey);
     }
 
     .hide {
