9785fe2e0cd0f66646591d5f93c824ba98eb8bff
EduardoMontanha <edumontanha14@gmail.com>
2022-07-07 22:15:53 -0300

    Propriedades dinamicas

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,33 @@
 import styled from 'styled-components';
 
-const ButtonStyle = styled.button`
+interface ButtonProps {
+    full?: boolean,
+    primary?: boolean
+}
 
+interface ComponentProps {
+    full?: boolean,
+    label: string,
+    primary?: boolean
+}
+
+const ButtonStyle = styled.button<ButtonProps>`
+    width: ${props => props.full ? "100%" : "auto"};
+    padding: 0.5rem 1rem;
+    border: none;
+    border-radius: 4px;
+    background-color: ${props => props.primary ? "red" : "blue"};
+    color: #FFFFFF;
+    font-weight: bold;
+    cursor: pointer;
 `;
 
-const Button = () => {
-    
+const Button = ({ full, label, primary }: ComponentProps) => {
+    const fullWidth = !!full ? full : false;
+    const primaryBtn = !!primary ? primary : false;
+
     return (
-        <ButtonStyle></ButtonStyle>
+        <ButtonStyle full={fullWidth} primary={primaryBtn}>{label}</ButtonStyle>
     );
 }
 
