f51f84c379f79afbc71bab2c299f3562ed72d803
EduardoMontanha <edumontanha14@gmail.com>
2022-07-09 13:48:13 -0300

    Change sample data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,14 +5,14 @@ const Home = () => {
 
     const product: ProductDetails = {
         creationDate: "2022-06-20T12:01:04.753Z",
-        salePrice: 170,
+        salePrice: 280,
         date: "2022-08-10T12:01:04.753Z",
         fullPrice: 290,
         id: 1,
         imageSrc: "https://www.tudodeviagem.com/wp-content/uploads/2021/12/Gruta-do-Lago-Azul-Daniel-De-Granville-2.jpg",
         location: "Bonito / MS",
         quantity: 29,
-        title: "Cascata da Borda Infinita",
+        title: "Canyons de Cambará",
     }
 
     return (
