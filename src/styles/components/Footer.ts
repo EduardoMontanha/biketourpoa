06bd4fb6f9cd6c8d43fbd6209197a4a130d13007
EduardoMontanha <edumontanha14@gmail.com>
2022-07-09 18:30:10 -0300

    Basic style for footer and header

---
+++ src/styles/components/Footer.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const FooterArea = styled.footer`
+    width: 100vw;
+    padding: 2rem 0;
+    background-color: var(--pink);
+    color: var(--white);
+`;
---
+++ src/styles/components/Header.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HeaderArea = styled.header`
+    width: 100vw;
+    height: 5rem;
+    padding: 1rem 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: var(--yellow);
+    box-shadow: 0 -8px 16px 4px #000000;
+`;
