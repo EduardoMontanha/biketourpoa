2ecc0b197c0d6f6879fcdbd8463fead846267229
EduardoMontanha <edumontanha14@gmail.com>
2022-07-13 18:56:29 -0300

    Added date to card

--- src/styles/components/ProductCard.ts
+++ src/styles/components/ProductCard.ts
@@ -21,18 +21,34 @@ export const Card = styled.div`
 
 export const Image = styled.div`
     text-align: center;
+    font-size: 0;
+    position: relative;
 
     & img {
         max-width: 100%;
-        max-height: 100%;
+        max-height: 170px;
         border-top-right-radius: 8px;
         border-top-left-radius: 8px;
+    }
+
+    .date {
+        width: 100%;
+        padding: 0.3rem 0;
+        background-color: var(--green);
+        position: absolute;
+        bottom: 0;
+        opacity: 0.9;
+        font-size: 1rem;
+        letter-spacing: 0.1rem;
+        font-family: var(--content-font-bold);
+        color: var(--white);
     }
 `;
 
 export const Location = styled.div`
     display: flex;
     align-items: center;
+    justify-content: center;
     margin-bottom: 1.2rem;
     font-size: 0.8rem;
     color: var(--grey);
@@ -66,16 +82,13 @@ export const Tags = styled.div`
     flex-wrap: wrap;
     align-items: center;
     justify-content: flex-start;
+    gap: 0.2rem;
+    margin-top: 0.2rem;
     font-size: 0.8rem;
-
-    &.no-tag {
-        margin-bottom: 25.5px;
-    }
 
     .tag {
         padding: 0.2rem 0.4rem;
         border-radius 4px;
-        margin: 0.1rem;
         display: inline-block;
         color: #FFFFFF;
         opacity: 0.8;
@@ -100,7 +113,7 @@ export const Tags = styled.div`
 `;
 
 export const Title = styled.h3`
-    margin: 0.2rem 0;
+    margin: 0.5rem 0 0.2rem;
     font-size: 1.8rem;
     text-align: center;
     color: var(--pink);
