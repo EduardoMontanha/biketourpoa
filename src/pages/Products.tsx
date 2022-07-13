62730d330067e19b683b60ee7dd9de6c58849bf2
EduardoMontanha <edumontanha14@gmail.com>
2022-07-13 17:43:12 -0300

    Mounting products dinamicaly

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -5,58 +5,66 @@ import { List } from "../styles/Products";
 
 const Products = () => {
 
-    const product: ProductDetails = {
-        creationDate: "2022-03-20T12:01:04.753Z",
-        salePrice: 290,
-        date: "2022-08-10T12:01:04.753Z",
-        fullPrice: 290,
-        id: 1,
-        imageSrc: "https://www.tudodeviagem.com/wp-content/uploads/2021/12/Gruta-do-Lago-Azul-Daniel-De-Granville-2.jpg",
-        location: "Bonito / MS",
-        quantity: 0,
-        title: "Canyons de Cambará",
-    }
-
-    const product2: ProductDetails = {
-        creationDate: "2022-06-20T12:01:04.753Z",
-        salePrice: 290,
-        // date: "2022-08-10T12:01:04.753Z",
-        fullPrice: 290,
-        id: 2,
-        imageSrc: "https://media-cdn.tripadvisor.com/media/photo-s/14/2d/19/44/mejor-uruguay.jpg",
-        location: "Montevideo / Uruguay",
-        quantity: 29,
-        title: "Feira do Mel, Rosca e Nata",
-    }
-
-    const product3: ProductDetails = {
-        creationDate: "2022-05-20T12:01:04.753Z",
-        salePrice: 280,
-        date: "2022-08-10T12:01:04.753Z",
-        fullPrice: 290,
-        id: 3,
-        imageSrc: "https://a.cdn-hotels.com/gdcs/production167/d783/dec17324-b0d9-428a-9b78-9f415d5db892.jpg",
-        location: "Bento Gonçalves / RS",
-        quantity: 9,
-        title: "Caminhos de Pedra e do Vinho",
-    }
+    const products: ProductDetails[] = [
+        {
+            creationDate: "2022-03-20T12:01:04.753Z",
+            salePrice: 290,
+            date: "2022-11-28T12:01:04.753Z",
+            fullPrice: 290,
+            id: 1,
+            imageSrc: "https://www.falandodeviagem.com.br/imagens19/10PasseiosCamabaraSul1.jpg",
+            location: "Cambará do Sul / RS",
+            quantity: 0,
+            title: "Canyons de Cambará",
+        },
+        {
+            creationDate: "2022-06-20T12:01:04.753Z",
+            salePrice: 290,
+            // date: "2022-08-10T12:01:04.753Z",
+            fullPrice: 290,
+            id: 2,
+            imageSrc: "https://media-cdn.tripadvisor.com/media/photo-s/14/2d/19/44/mejor-uruguay.jpg",
+            location: "Montevideo / Uruguay",
+            quantity: 29,
+            title: "Uruguay Incrível",
+        },
+        {
+            creationDate: "2022-05-20T12:01:04.753Z",
+            salePrice: 110,
+            date: "2022-08-09T12:01:04.753Z",
+            fullPrice: 130,
+            id: 3,
+            imageSrc: "https://a.cdn-hotels.com/gdcs/production167/d783/dec17324-b0d9-428a-9b78-9f415d5db892.jpg",
+            location: "Bento Gonçalves / RS",
+            quantity: 9,
+            title: "Caminhos de Pedra e do Vinho",
+        },
+        {
+            creationDate: "2022-05-20T12:01:04.753Z",
+            // salePrice: 290,
+            date: "2022-08-09T12:01:04.753Z",
+            fullPrice: 320,
+            id: 4,
+            imageSrc: "https://www.viajali.com.br/wp-content/uploads/2017/12/serra-do-rio-do-rastro-10.jpg",
+            location: "Lauro Muller / SC",
+            quantity: 19,
+            title: "Serra do Rio do Rastro",
+        }
+    ];
 
     return(
         <main>
             <div className="container">
                 <List>
-                    <ProductCard product={product} />
-                    <ProductCard product={product2} />
-                    <ProductCard product={product3} />
-                    <ProductCard product={product} />
-                    <ProductCard product={product2} />
-                    <ProductCard product={product3} />
-                    <ProductCard product={product} />
-                    <ProductCard product={product2} />
-                    <ProductCard product={product3} />
-                    <ProductCard product={product} />
-                    <ProductCard product={product2} />
-                    <ProductCard product={product3} />
+                    {products.map((product, key) => 
+                        <ProductCard product={product} key={key} />
+                    )}
+                    {products.map((product, key) => 
+                        <ProductCard product={product} key={key} />
+                    )}
+                    {products.map((product, key) => 
+                        <ProductCard product={product} key={key} />
+                    )}
                 </List>
             </div>
         </main>
