c19067e4b02d9e4e06e56b8f7293ffb2e22386a8
EduardoMontanha <edumontanha14@gmail.com>
2022-07-27 10:53:11 -0300

    Header base

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,31 @@
-import { HeaderArea } from "../styles/components/Header";
+import { BurgerButton, HeaderArea, Navigation } from "../styles/components/Header";
+import logo from '../assets/images/logo-big.png';
+import { Link } from "react-router-dom";
 
 const Header = () => {
 
     return (
         <HeaderArea>
             <div className="container">
-                header
+                <Link to="/">
+                    <img src={logo} alt="" />
+                </Link>
+                
+                <BurgerButton />
+                
+                <Navigation>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/roteiros">Roteiros</Link>
+                        </li>
+                        <li>
+                            <Link to="/contato">Contato</Link>
+                        </li>
+                    </ul>
+                </Navigation>
             </div>
         </HeaderArea>
     );
--- src/styles/components/Header.ts
+++ src/styles/components/Header.ts
@@ -1,13 +1,52 @@
 import styled from 'styled-components';
 
+export const BurgerButton = styled.button`
+
+    @media all and (min-width: 1024px) {
+        display: none;
+    }
+`;
+
 export const HeaderArea = styled.header`
     width: 100%;
     height: 5rem;
-    padding: 1rem 0;
     position: fixed;
     top: 0;
     left: 0;
     z-index: 1000;
     background-color: var(--yellow);
     box-shadow: 0 -8px 16px 4px #000000;
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+    }
+
+    img {
+        height: 3rem;
+    }
+`;
+
+export const Navigation = styled.nav`
+    display: none;
+
+    ul {
+        display: flex;
+    }
+
+    li a {
+        color: var(--pink);
+        font-family: var(--content-font-bold);
+        font-size: 1.4rem;
+    }
+
+    li + li {
+        margin-left: 1rem;
+    }
+
+    @media all and (min-width: 1024px) {
+        display: inline-block;
+    }
 `;
